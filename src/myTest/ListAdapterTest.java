c24bc4f8f3cf9b559062e7edae79d8fd1eda79e3
aletrevi99 <alessandrotrevisan99@gmail.com>
2021-05-09 17:08:10 +0200

    created ListAdapterTest.java

---
+++ src/myTest/ListAdapterTest.java
@@ -0,0 +1,135 @@
+package myTest;
+
+import myAdapter.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListAdapterTest
+{
+
+    @Test
+    public void add()
+    {
+    }
+
+    @Test
+    public void testAdd()
+    {
+    }
+
+    @Test
+    public void addAll()
+    {
+    }
+
+    @Test
+    public void testAddAll()
+    {
+    }
+
+    @Test
+    public void clear()
+    {
+    }
+
+    @Test
+    public void contains()
+    {
+    }
+
+    @Test
+    public void containsAll()
+    {
+    }
+
+    @Test
+    public void testEquals()
+    {
+    }
+
+    @Test
+    public void testHashCode()
+    {
+    }
+
+    @Test
+    public void get()
+    {
+    }
+
+    @Test
+    public void indexOf()
+    {
+    }
+
+    @Test
+    public void isEmpty()
+    {
+    }
+
+    @Test
+    public void iterator()
+    {
+    }
+
+    @Test
+    public void lastIndexOf()
+    {
+    }
+
+    @Test
+    public void listIterator()
+    {
+    }
+
+    @Test
+    public void testListIterator()
+    {
+    }
+
+    @Test
+    public void remove()
+    {
+    }
+
+    @Test
+    public void testRemove()
+    {
+    }
+
+    @Test
+    public void removeAll()
+    {
+    }
+
+    @Test
+    public void retainAll()
+    {
+    }
+
+    @Test
+    public void set()
+    {
+    }
+
+    @Test
+    public void size()
+    {
+    }
+
+    @Test
+    public void subList()
+    {
+    }
+
+    @Test
+    public void toArray()
+    {
+    }
+
+    @Test
+    public void testToArray()
+    {
+    }
+}
